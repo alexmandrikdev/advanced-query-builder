79e31c823a090fec9252020bd919d0d204d00e8f
Alex Andras Mandrik <alexmandrik@pm.me>
2024-02-25 20:32:26 +0100

    Setup testing database

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,29 @@
 
 namespace Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpDatabase();
+    }
+
+    protected function setUpDatabase()
+    {
+        tap($this->app['db']->connection()->getSchemaBuilder(), function ($schema) {
+            $schema->create('markets', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->timestamps();
+            });
+        });
+    }
 }
